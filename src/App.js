856879a82610c6545499e28c020f496c1c106388
huynhtrongtanh <huynhtrongtanh.htt@gmail.com>
2024-07-31 15:10:18 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, HashRouter } from 'react-router-dom';
 
 
-import FormLogIn   from './components/FormLogIn';
-import FormSignUp  from './components/FormSignUp';
+import FormLogIn from './components/FormLogIn';
+import FormSignUp from './components/FormSignUp';
 import MainPage from './components/MainPage';
 
 function App() {
@@ -13,18 +13,20 @@ function App() {
         <Router>
             <div>
                 <header className="app-header">
-                  <Link className="navbar-brand">
-                    <img src={`${process.env.PUBLIC_URL}/Logo.png`} alt="Logo" className="app-logo" />
-                    <h1>Attendance Management</h1>
-                  </Link>
+                    <Link className="navbar-brand">
+                        <img src={`${process.env.PUBLIC_URL}/Logo.png`} alt="Logo" className="app-logo" />
+                        <h1>Attendance Management</h1>
+                    </Link>
                 </header>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<FormLogIn />} />
-                        <Route path="/signup" element={<FormSignUp />} />
-                        <Route path="/login" element={<FormLogIn />} />
-                        <Route path="/main" element={<MainPage />} />
-                    </Routes>
+                    <HashRouter>
+                        <Routes>
+                            <Route path="/" element={<FormLogIn />} />
+                            <Route path="/signup" element={<FormSignUp />} />
+                            <Route path="/login" element={<FormLogIn />} />
+                            <Route path="/main" element={<MainPage />} />
+                        </Routes>
+                    </HashRouter>
                 </main>
             </div>
         </Router>
